16043738b4023b9faefa4b0b51190e9e2ab28c90
Sean Staz <seanstaz52@gmail.com>
2015-06-28 17:37:36 -0500

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -41,5 +41,4 @@ for turn in range(4):
             board[guess_row][guess_col] = "X"
         if turn == 3:
             print "Game Over"
-        # Print (turn + 1) here!
         print_board(board)
